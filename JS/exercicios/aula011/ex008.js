56c8a122269f31f9a83a565f16d39d618010a237
MarioSilva <calebemp33@gmail.com>
2026-07-14 18:12:33 -0300

    Condicões if e else

---
+++ JS/exercicios/aula011/ex008.js
@@ -0,0 +1,6 @@
+var vel = 60.00001
+console.log(`Atenção! a velocidade maxima da via é de 60km/h`)
+if (vel > 60) {
+    console.log(`A sua velocidade atual é de ${vel} Reduza! Fiscalização logo a frente"`)
+    console.log(`Você foi MULTADO`)
+}
