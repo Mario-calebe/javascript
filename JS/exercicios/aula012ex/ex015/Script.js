8915847e0a644e66a7099e7f441003a1465f6ce2
MarioSilva <calebemp33@gmail.com>
2026-07-17 13:24:11 -0300

    fotos

---
+++ JS/exercicios/aula012ex/ex015/Script.js
@@ -0,0 +1,9 @@
+function verificar() {
+    var data = new Date()
+    var ano = data.getFullYear()
+    var fano = window.document.getElementById('itxtano')
+    var res = document.getElementById('res')
+    if (fano < ano) {
+        
+    }
+}
