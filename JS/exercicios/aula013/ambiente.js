564613da35881abb9754ae883dfdf6671cc472e9
MarioSilva <calebemp33@gmail.com>
2026-07-23 18:13:23 -0300

    while e  do...while

---
+++ JS/exercicios/aula013/ambiente.js
@@ -0,0 +1,25 @@
+var c = 1
+do{
+    console.log(`passo ${c}`);
+    
+
+}while (c <= 6) 
+    
+
+
+
+
+/*var c = 1
+while (c <= 6) {
+    console.log('hello,word!');
+    c++
+}
+*/
+/*
+ console.log('Tudo bem?');
+ console.log('Tudo bem?');
+ console.log('Tudo bem?');
+ console.log('Tudo bem?');
+ console.log('Tudo bem?');
+ console.log('Tudo bem?');
+*/ 
