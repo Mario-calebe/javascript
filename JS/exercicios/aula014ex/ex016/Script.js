4c9a1bb78cc42d6fc5f3b2df2a99984eec4f9733
MarioSilva <calebemp33@gmail.com>
2026-07-25 16:26:07 -0300

    Quebrando a cabeça com ex016

--- JS/exercicios/aula014ex/ex016/Script.js
+++ JS/exercicios/aula014ex/ex016/Script.js
@@ -1,24 +1,33 @@
 function contar() {
-    var inc = window.document.getElementById('inicio')
-    var fim = window.document.getElementById('fim')
-    var step = window.document.getElementById('passo')
-    var n1 = Number(inc.value)//Isto é o valor que o cliente digitar!!!
-    var n2 = Number(fim.value)//Isto é o valor que o cliente digitar!!!
-    var n3 = Number(step.value)//Isto é o valor que o cliente digitar!!!
-    var res = window.document.getElementById('res')
-    if (inc.value.length == 0 || fim.value.length == 0) {
-        res.innerHTML = "IMPOSIVEL CONTAR"
-    }else if(n3 == 0){
-        alert('Passo inválido! Considerando PASSO 1')
-        step.value = 1
-        n3 = 1
-        res.innerHTML = ''
-        res.innerHTML += `<h1 class = "hres">Contando: </h1>`
-        while (n1 <= n2) {
-            res.innerHTML += `${n1} → `
-            n1 = n1 + n3
-        }
-        res.innerHTML += `Passos Encerrados!`
-    }
+    let inc = document.getElementById('inicio')
+    let fim = document.getElementById('fim')
+    let passo = document.getElementById('passo')
+    let res = document.getElementById('res')
     
-}   
+    if (inc.value.length == 0 || fim.value.length == 0) {
+        alert('[Erro] Valores invalidos!')
+        res.innerHTML = `Imposivel Contar!`
+    } else {
+
+        let i = Number(inc.value)
+        let f = Number(fim.value)
+        let p = Number(passo.value)
+        res.innerHTML = `<h1 class = "hres" >Contando:</h1>`
+
+        if(passo.value == 0 || p <= 0){
+            alert(`Passos invalidos!\nConsiderando PASSO 1`)
+            p = 1
+            passo.value = 1
+        }
+        if(i > f){//Contagem Regressiva
+            for(let c = i; c >= f; c -= p){
+            res.innerHTML += `${c}👉`
+            }
+        }else{// Contagem Crescente
+            for(let c = i; c <= f; c += p){
+            res.innerHTML += `${c}👉`
+            }
+        }
+    res.innerHTML += `🚧` 
+    }
+}
