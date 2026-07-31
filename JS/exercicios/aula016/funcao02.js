a208bd0b1cdd754c12a72f6f0169c0a82c9e012d
MarioSilva <calebemp33@gmail.com>
2026-07-30 22:18:51 -0300

    abiente .js

---
+++ JS/exercicios/aula016/funcao02.js
@@ -0,0 +1,7 @@
+function soma(n1=0, n2=0) {
+    return n1 + n2
+}
+
+let som = soma( 1 ,)
+console.log(som);
+ 
---
+++ JS/exercicios/aula016/funcao03.js
@@ -0,0 +1,5 @@
+let v = function (x) {
+    return x*2
+}
+let chamada = v(5)
+console.log(chamada);
